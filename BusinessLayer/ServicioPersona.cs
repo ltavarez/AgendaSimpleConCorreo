e481ab5ae2d777c31aae549b2e8b2e09168fb8c4
Leonardo Tavarez <leonardotv.93@gmail.com>
2020-11-18 20:02:54 -0400

    Initial commit

---
+++ BusinessLayer/ServicioPersona.cs
@@ -0,0 +1,64 @@
+﻿using Database;
+using Database.Modelos;
+using EmailHandler;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BusinessLayer
+{
+    public class ServicioPersona : IServicios<Persona>
+    {
+
+        private readonly PersonaRepository _repository;
+        private readonly EmailSender _emailSender;
+
+        public ServicioPersona(SqlConnection connection)
+        {
+            _repository = new PersonaRepository(connection);
+            _emailSender = new EmailSender();
+
+        }
+
+        public bool Add(Persona item)
+        {
+            bool response = _repository.Add(item);
+
+            if (response)
+            {
+                _emailSender.SendEmail("phpitladiplomado@gmail.com", "Nuevo contacto", "Se ha agregado este neuvo contacto: " + item.Nombre + " " + item.Apellido);
+            }
+
+            return response;
+        }
+
+        public bool Edit(Persona item)
+        {
+            return _repository.Edit(item);
+        }
+
+        public bool Delete(int id)
+        {
+            return _repository.Delete(id);
+        }
+
+        public Persona GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public DataTable GetAll()
+        {
+            return _repository.List();
+        }
+
+        public bool SavePhoto(int id, string destination)
+        {
+            return _repository.SavePhoto(id, destination);
+        }
+
+        public int GetLastId()
+        {
+            return _repository.GetLastId();
+        }
+    }
+}
---
+++ EmailHandler/EmailSender.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailHandler
+{
+    public class EmailSender
+    {
+
+        public void SendEmail(string to,string subject,string body)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient smtpServer = new SmtpClient(EmailSetting.Default.SmtpServer);
+
+                mail.From = new MailAddress(EmailSetting.Default.From);
+                mail.To.Add(to);
+                mail.Subject = subject;
+                mail.Body = body;
+
+                smtpServer.Port = EmailSetting.Default.Port;
+                smtpServer.Credentials = new NetworkCredential(EmailSetting.Default.From, EmailSetting.Default.Paswword);
+                smtpServer.EnableSsl = true;
+
+                smtpServer.Send(mail);
+                Console.WriteLine("Se envio el correo");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+        }
+
+    }
+}
